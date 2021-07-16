99c733fcdadc5b8c8b004b36f8c55f46776ffec7
James Grande <james.grande6@gmail.com>
2021-07-15 19:21:41 -1000

    Fix display value error

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ const operatorButtons = document.querySelectorAll(".operator");
 const displayDiv = document.querySelector(".display");
 const operatorsDiv = document.querySelector(".operators");
 
-
 onOperatorClick();
 onNumClick();
 onEqualsClick();
 
-// Bug: becoming empty after doing equals then operator
 function onOperatorClick() {
-
     operatorButtons.forEach(operatorButton => {
         operatorButton.addEventListener("click", () => {
 
@@ -25,7 +22,9 @@ function onOperatorClick() {
             if (!(operatorsDiv.classList.contains("active"))) {
                 operatorsDiv.classList.add("active");
             }
-
+            if (operator == null) {
+                operator = operatorButton.id;
+            }
             if (displayDiv.id == "reset") {
                 operator = operatorButton.id;
                 return;
@@ -36,14 +35,15 @@ function onOperatorClick() {
                 console.log("operator = " + operator);
                 console.log("operands[0] = " + operands[0]);
                 console.log("operands[1] = " + operands[1]);
+
                 let results = operate(operator, operands[0], operands[1]);
                 display(results);
                 operands.pop();
                 operands[0] = results;
             }
             operator = operatorButton.id;
-
             displayDiv.id = "reset";
+            
         })
     });
 }
@@ -52,30 +52,31 @@ function onEqualsClick() {
     const equalsButton = document.querySelector(".equals");
 
     equalsButton.addEventListener("click", () => {
+        disableOperators();
 
-        if (operatorsDiv.classList.contains("active")) {
-            operatorButtons.forEach(operatorButton => operatorButton.classList.remove("active"));
-            operatorsDiv.classList.remove("active");
-        }
-
-        if (operator == null) {
-            return;
-        }
-
-        if (displayDiv.id == "reset") {
+        if (operator == null || displayDiv.id == "reset") {
             return;
         }
         operands.push(Number(displayDiv.textContent));
 
-        if (operands.length == 2) {
+        if (operands.length === 2) {
             let results = operate(operator, operands[0], operands[1]);
+
             display(results);
             operands.pop();
             operands[0] = results;
         }
-        displayDiv.id = "reset";
+
         operator = null;
+        displayDiv.id = "reset";
     });
+}
+
+function disableOperators() {
+    if (operatorsDiv.classList.contains("active")) {
+        operatorButtons.forEach(operatorButton => operatorButton.classList.remove("active"));
+        operatorsDiv.classList.remove("active");
+    }
 }
 
 function onNumClick() {
@@ -94,15 +95,12 @@ function onNumClick() {
                 display(newNum);
                 displayDiv.id = newNum;
             }
-            console.log(displayDiv.id);
         });
     });
 }
 
-
 function display(numText) {
     const displayDiv = document.querySelector(".display");
-
     displayDiv.textContent = numText;
 }
 
